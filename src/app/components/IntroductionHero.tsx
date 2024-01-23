b60b2a2ed9f11c0dc1dbf0be63ae19c21f96edc8
HassenH1 <hasansaid51@gmail.com>
2024-01-22 23:16:34 -0800

    added carousel

--- src/app/components/IntroductionHero.tsx
+++ src/app/components/IntroductionHero.tsx
@@ -1,38 +1,77 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function IntroductionHero() {
-  const [image, setImage] = useState(1);
-
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setImage((prev) => {
-        if (prev === 6) {
-          return 1;
-        }
-        return prev + 1;
-      });
-    }, 4000);
-
-    return function cleanup() {
-      console.log(`Clearing ${timerId}`);
-      clearInterval(timerId);
-    };
-  }, []);
-
   return (
     <main className="flex-1 flex p-4 justify-center items-center">
       <div className="md:h-[600px] md:w-[700px] h-[400px] w-[400px] relative">
-        <Image
+        <Swiper
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          centeredSlides
+          scrollbar={{ draggable: true }}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          className="w-full h-full"
+          loop
+          speed={3000}
+        >
+          <SwiperSlide>
+            <Image
+              height={0}
+              width={0}
+              className="w-full h-full object-cover object-center hover:blur-sm block"
+              src="/1.jpg"
+              alt="Maquis Scott photography"
+              sizes="100vw"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              height={0}
+              width={0}
+              className="w-full h-full object-cover object-center hover:blur-sm block"
+              src="/2.jpg"
+              alt="Maquis Scott photography"
+              sizes="100vw"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              height={0}
+              width={0}
+              className="w-full h-full object-cover object-center hover:blur-sm block"
+              src="/3.jpg"
+              alt="Maquis Scott photography"
+              sizes="100vw"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              height={0}
+              width={0}
+              className="w-full h-full object-cover object-center hover:blur-sm block"
+              src="/4.jpg"
+              alt="Maquis Scott photography"
+              sizes="100vw"
+            />
+          </SwiperSlide>
+        </Swiper>
+        {/* <Image
           height={0}
           width={0}
           className="w-full h-full object-cover object-center hover:blur-sm block"
           src={`/${image}.jpg`}
           alt="Maquis Scott photography"
           sizes="100vw"
-        />
+        /> */}
         <span className="absolute translate-y-1/2 top-1/2 -right-[100px] sm:-right-36 transform -rotate-90 text-[10px] sm:text-sm -z-40">
           <strong>ALL IMAGES</strong> &copy; 2023 <strong>MAQUIS SCOTT</strong>
         </span>
