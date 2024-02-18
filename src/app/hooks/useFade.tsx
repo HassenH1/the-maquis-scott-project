057445323e2ff32e726c1680aa1bbf63387fab7b
HassenH1 <hasansaid51@gmail.com>
2024-02-18 10:15:29 -0800

    first iteration of intro screen made

---
+++ src/app/hooks/useFade.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+const useFade = (initial: boolean = false) => {
+  const [show, setShow] = useState(initial);
+  const [isVisible, setVisible] = useState(show);
+
+  // Update visibility when show changes
+  useEffect(() => {
+    if (show) setVisible(true);
+  }, [show]);
+
+  // When the animation finishes, set visibility to false
+  const onAnimationEnd = () => {
+    if (!show) setVisible(false);
+  };
+
+  const style = { animation: `${show ? "fadeIn" : "fadeOut"} 3s ease-out 1s` };
+  // const style = { animation: `${show ? "fadeIn" : "fadeOut"} .3s` };
+
+  // These props go on the fading DOM element
+  const fadeProps: any = {
+    style,
+    onAnimationEnd,
+  };
+
+  return [isVisible, setShow, fadeProps];
+};
+
+export default useFade;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,51 @@
+"use client";
+
+import Image from "next/image";
 import SwiperSlider from "./components/SwiperSlider";
 import Box from "./components/box";
+import Footer from "./components/footer";
+import useFade from "./hooks/useFade";
+import { useEffect } from "react";
 
 export default function Home() {
+  const [isVisible, setVisible, fadeProps] = useFade();
   const year = new Date().getFullYear();
 
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true);
+    }, 2000);
+  }, []);
+
   return (
-    <main className="flex-1 flex p-4 justify-center items-center">
-      <Box>
-        <SwiperSlider />
-        <span className="absolute translate-y-1/2 top-1/2 -right-[100px] sm:-right-36 transform -rotate-90 text-[10px] sm:text-sm -z-40">
-          <strong>ALL IMAGES</strong> &copy; {year}{" "}
-          <strong>MAQUIS SCOTT</strong>
-        </span>
-      </Box>
-    </main>
+    <>
+      {isVisible ? (
+        <>
+          <header className="p-4" />
+          <main className="flex-1 flex p-4 justify-center items-center">
+            <Box>
+              <SwiperSlider />
+              <span className="absolute translate-y-1/2 top-1/2 -right-[100px] sm:-right-36 transform -rotate-90 text-[10px] sm:text-sm -z-40">
+                <strong>ALL IMAGES</strong> &copy; {year}{" "}
+                <strong>MAQUIS SCOTT</strong>
+              </span>
+            </Box>
+          </main>
+          <Footer />
+        </>
+      ) : (
+        <div className="w-full h-full" {...fadeProps}>
+          <Image
+            src="/LANDING LOGO.jpg"
+            height={0}
+            width={0}
+            className="w-full h-full object-cover object-center block"
+            alt="Maquis Scott photography"
+            sizes="100vw"
+            priority
+          />
+        </div>
+      )}
+    </>
   );
 }
