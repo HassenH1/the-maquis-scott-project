acd835f4dbf12c69f86836dcae3467a963fcb7a1
HassenH1 <hasansaid51@gmail.com>
2024-02-19 18:58:52 -0800

    added grid to footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,18 +2,24 @@
 
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Footer() {
+  const pathname = usePathname();
+  const isCreditsPath = pathname === "/credits" ? true : false;
   const year = new Date().getFullYear();
 
   return (
-    <footer className="md:flex">
-      <div className="md:inline-block md:w-2/5 md:text-center">
-        <Link className="text-xs font-bold md:mr-32 ml-7" href="/credits">
-          credits
+    <footer className="grid grid-cols-1	md:grid-cols-4">
+      <div className="md:text-center md:mx-auto mx-12">
+        <Link
+          className="text-xs font-bold"
+          href={isCreditsPath ? "/" : "/credits"}
+        >
+          {isCreditsPath ? "home" : "credits"}
         </Link>
       </div>
-      <div className="md:inline-block md:text-left text-center md:w-3/5">
+      <div className="text-center col-span-2">
         <span className="text-xs">
           <strong>ALL RIGHTS RESERVED</strong> &copy; 2013 - {year}{" "}
           <strong>STUDIO EXIT LLC</strong>
@@ -24,3 +30,20 @@ function Footer() {
 }
 
 export default Footer;
+
+// <footer className="md:flex">
+// <div className="md:inline-block md:w-2/5 md:text-center">
+//   <Link
+//     className="text-xs font-bold md:mr-32 ml-7"
+//     href={isCreditsPath ? "/" : "/credits"}
+//   >
+//     {isCreditsPath ? "home" : "credits"}
+//   </Link>
+// </div>
+// <div className="md:inline-block md:text-left text-center md:w-3/5">
+//   <span className="text-xs">
+//     <strong>ALL RIGHTS RESERVED</strong> &copy; 2013 - {year}{" "}
+//     <strong>STUDIO EXIT LLC</strong>
+//   </span>
+// </div>
+// </footer>
