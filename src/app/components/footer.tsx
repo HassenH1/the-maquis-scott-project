be5e6a146015ef770a8bdcedd87f2c5ac269dd49
HassenH1 <hasansaid51@gmail.com>
2024-03-11 21:58:01 -0700

    ...

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,27 +1,16 @@
-"use client";
-
 import React from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 function Footer() {
-  const pathname = usePathname();
-  const isCreditsPath = pathname === "/credits" ? true : false;
-  const year = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="grid grid-cols-1	md:grid-cols-4">
-      <div className="md:text-center md:mx-auto mx-12">
-        <Link
-          className="text-xs font-bold"
-          href={isCreditsPath ? "/" : "/credits"}
-        >
-          {isCreditsPath ? "home" : "credits"}
-        </Link>
-      </div>
+      <div className="md:text-center md:mx-auto mx-12"></div>
       <div className="text-center col-span-2">
         <span className="text-xs">
-          <strong>ALL RIGHTS RESERVED</strong> &copy; 2013 - {year}{" "}
+          <span className="font-medium">
+            &copy; {currentYear} COPYRIGHTS by
+          </span>{" "}
           <strong>STUDIO EXIT LLC</strong>
         </span>
       </div>
@@ -31,18 +20,13 @@ function Footer() {
 
 export default Footer;
 
-// <footer className="md:flex">
-// <div className="md:inline-block md:w-2/5 md:text-center">
-//   <Link
-//     className="text-xs font-bold md:mr-32 ml-7"
-//     href={isCreditsPath ? "/" : "/credits"}
-//   >
-//     {isCreditsPath ? "home" : "credits"}
-//   </Link>
-// </div>
-// <div className="md:inline-block md:text-left text-center md:w-3/5">
+// <footer className="grid grid-cols-1	md:grid-cols-4">
+// <div className="md:text-center md:mx-auto mx-12"></div>
+// <div className="text-center col-span-2">
 //   <span className="text-xs">
-//     <strong>ALL RIGHTS RESERVED</strong> &copy; 2013 - {year}{" "}
+//     <span className="font-medium">
+//       &copy; {currentYear} COPYRIGHTS by
+//     </span>{" "}
 //     <strong>STUDIO EXIT LLC</strong>
 //   </span>
 // </div>
