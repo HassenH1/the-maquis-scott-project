a2e087c6b916e91d55681da6717e1024cf9074a0
Hassen Hassen <48224444+HassenH1@users.noreply.github.com>
2024-09-04 22:38:53 -0700

    updates... (#20)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
-import Box from "../components/box";
 import SwiperSlides from "../components/SwiperSlides";
+import Splashscreen from "../components/splashscreen";
 const SlideShow = dynamic(() => import('../components/slideShow'), { ssr: false })
 
 export default function Home() {
@@ -10,16 +10,16 @@ export default function Home() {
     <>
       <section id="top" className="h-screen flex flex-col justify-center">
         <div className="flex-1 flex flex-col justify-center items-center">
-          <Box>
-            {/* <SwiperSlider */}
+          <div className="md:h-[600px] md:w-[700px] h-[400px] w-[320px] relative">
+            <Splashscreen />
             <SwiperSlides />
             <span className="absolute translate-y-1/2 top-1/2 -right-[100px] sm:-right-36 transform -rotate-90 text-[10px] sm:text-sm -z-40">
               <strong>ALL IMAGES</strong> &copy; {year}{" "}
               <strong>MAQUIS SCOTT</strong>
             </span>
-          </Box>
+          </div>
         </div>
-        <div className="flex-2 container mx-auto">
+        <div className="flex-2 container mx-auto pb-9">
           <a href="#more" className="transition-all duration-300 ease-in-out no-underline text-sm hover:line-through">MORE</a>
         </div>
       </section>
--- src/app/components/splashscreen.tsx
+++ src/app/components/splashscreen.tsx
@@ -1,33 +1,20 @@
-// 'use client'
-
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import localFont from 'next/font/local'
 
 const myFont = localFont({
   src: '../fonts/Narifah.woff2',
 })
+
 function Splashscreen() {
-  // const splashRef = useRef<HTMLElement>(null);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (splashRef && splashRef.current) {
-  //       splashRef.current.style.display = 'none';
-  //     }
-  //   }, 4000);
-  // }, []);
-
-
   return (
-    <main className="w-full h-full fixed z-50">
-      {/* <main className="w-full h-full fixed z-50 bg-white" ref={splashRef} id="logo" style={{ animation: "fadeOut 4s ease-out", animationDelay: "2s" }}> */}
-      <div className="w-full md:w-2/4 mx-auto h-full">
-        <div className="flex justify-center h-full flex-col font-extrabold ml-10 md:ml-0">
-          <p className="text-[10px] ml-32">VISUAL LANGUAGE BY</p>
+    <main className="w-full h-full absolute z-50 mix-blend-difference text-white">
+      <div className="w-full md:w-3/4 h-full">
+        <div className="flex justify-center h-full flex-col font-extrabold md:ml-10 ml-0">
+          <p className="text-base ml-32">visual language by...</p>
           <br />
           <br />
           <br />
-          <div className="text-5xl flex flex-col  tracking-wide">
+          <div className="text-4xl md:text-5xl flex flex-col  tracking-wide">
             <span>MA<span className={myFont.className}>Q</span>UIS</span>
             <span className="ml-6">
               SCOTT<sup className="text-[20px] relative align-baseline -top-6">&reg;</sup>
