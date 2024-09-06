b74ddc3afde6bab2e9d1828bbc53d03b0e92a1bc
HassenH1 <hasansaid51@gmail.com>
2024-09-06 10:42:32 -0700

    ...

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           </div>
         </div>
         <div className="flex-2 container mx-auto pb-9">
-          <a href="#more" className="transition-all duration-300 ease-in-out no-underline text-sm hover:line-through">MORE</a>
+          <a href="#more" className="transition-all duration-300 ease-in-out no-underline text-sm hover:line-through ml-4">MORE</a>
         </div>
       </section>
       <section id="more" className="h-screen">
