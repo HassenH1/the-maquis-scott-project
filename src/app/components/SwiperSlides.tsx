4a8e647a56677b768fe8e4f4a88b92ee7500acfa
Hassen Hassen <48224444+HassenH1@users.noreply.github.com>
2024-10-02 18:51:14 -0700

    added some animation (#25)

--- src/app/components/SwiperSlides.tsx
+++ src/app/components/SwiperSlides.tsx
@@ -5,37 +5,52 @@ import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { images } from "../../../lib/images";
+import { motion } from "framer-motion"
 
 function SwiperSlides() {
   return (
-    <Swiper
-      centeredSlides
-      scrollbar={{ draggable: true }}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: "20%"
       }}
-      speed={1500}
-      loop
-      modules={[Autoplay]}
-      className="relative h-full w-full"
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1 }}
+      animate={{
+        x: "0%",
+        y: "0%",
+      }}
     >
-      {images.map((image, idx) => {
-        return (
-          <SwiperSlide key={idx}>
-            <Image
-              height={624}
-              width={700}
-              src={image}
-              alt="Maquis Scott photography"
-              placeholder="blur"
-              className="w-full h-full object-cover"
-            // sizes="(min-width: 800px) 700px, 320px"
-            />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+      <Swiper
+        centeredSlides
+        scrollbar={{ draggable: true }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        speed={1500}
+        loop
+        modules={[Autoplay]}
+        className="relative h-full w-full"
+      >
+        {images.map((image, idx) => {
+          return (
+            <SwiperSlide key={idx}>
+              <Image
+                height={624}
+                width={700}
+                src={image}
+                alt="Maquis Scott photography"
+                placeholder="blur"
+                className="w-full h-full object-cover"
+              // sizes="(min-width: 800px) 700px, 320px"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </motion.div>
   );
 }
 
